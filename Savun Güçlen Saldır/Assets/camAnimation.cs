f340cc8a2c5b9a54d61229b674262922e3721400
SONSUZLUKxd <107800534+SONSUZLUKxd@users.noreply.github.com>
2022-06-28 18:41:10 +0300

    Barı düzeltim
    yerini düzeltim afied

---
+++ Savun Güçlen Saldır/Assets/camAnimation.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class camAnimation : MonoBehaviour
+{
+    public CharacterController playerController;
+    public Animation anim;
+    private bool isMoving;
+
+    private bool left;
+    private bool right;
+    void CameraAnimationa()
+    {
+        if(playerController.isGrounded == true)
+        {
+            if(isMoving == true)
+            {
+                if(left == true)
+                {
+                    if (!anim.isPlaying)
+                    {
+                        anim.Play("walkleft");
+                        left = false;
+                        right = true;
+                    }
+                }
+            }
+        }
+    }
+    
+    void Start()
+    {
+        left = true;
+        right = false;
+        
+    }
+
+   
+    void Update()
+    {
+        float ınputX = Input.GetAxis("Horizontel");
+        float InputY = Input.GetAxis("Vertical");
+
+        if (ınputX != 0 || InputY != 0)
+        {
+            isMoving = true;
+        }
+        else if (ınputX == 0 && InputY == 0)
+        {
+            isMoving = false;
+        }
+        CameraAnimationa();
+    }
+        
+    
+}
